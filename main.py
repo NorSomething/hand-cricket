d819c613e48c36a6953ca9e82e6aac18128f64a2
nirmal <mcnirmalkumar00@gmail.com>
2025-12-07 16:08:37 +0530

    added number of fingers up:

--- main.py
+++ main.py
@@ -4,6 +4,30 @@ import numpy as np
 import uuid #to generate unique unifrom id => random string for image name? (for no overlap)
 import os
 
+def count_fingers(hand_landmarks):
+    
+    fingertips = [4, 8, 12, 16, 20] #4 is thumb
+    lower_joints = [3, 6, 10, 14, 18] #3 is thumb
+    fingers = []
+
+    #landmark.x .y and .z are the position in x, y and depth from camera
+
+    #note : in image coordinate systems, higher y means lower on the screen (opposite of graphs).
+
+    for tip, joint in zip(fingertips[1:], lower_joints[1:]):
+        if hand_landmarks.landmark[tip].y < hand_landmarks.landmark[joint].y:
+            fingers.append(1)  # finger up
+        else:
+            fingers.append(0)  # finger down
+
+    #for thumb
+    if hand_landmarks.landmark[4].x > hand_landmarks.landmark[3].x:
+        fingers.append(1)
+    else:
+        fingers.append(0)
+
+    return sum(fingers) #number of fingers up
+
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands #that hand diagram
 
@@ -29,11 +53,15 @@ with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence=0.5)
 
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        print(results)
+        #print(results)
 
         #rendering results 
         if results.multi_hand_landmarks: #if we actually got some results
             for num, hand in enumerate(results.multi_hand_landmarks):
+
+                finger_count = count_fingers(hand)
+                print("Number of Fingers Up : ", finger_count)
+
                 mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
 
         #cv2.imshow('Video Feed', frame) #frame is raw webcam feed, but we need with joints
@@ -44,3 +72,5 @@ with mp_hands.Hands(min_detection_confidence = 0.8, min_tracking_confidence=0.5)
 
 cap.release()
 cv2.destroyAllWindows()
+
+
